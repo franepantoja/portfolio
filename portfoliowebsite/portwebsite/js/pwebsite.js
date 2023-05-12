d28bae95a3f5a3b29e5dcd081d584810e018ca46
franepantoja <117616872+franepantoja@users.noreply.github.com>
2023-05-12 15:17:15 -0500

    Add files via upload

---
+++ portfoliowebsite/portwebsite/js/pwebsite.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+   // brochure
+   $(".p1").mouseenter(function() {
+
+      $(".h1").removeClass("hide");
+      $(".h1").addClass("showw");
+   });
+
+   $(".p1").mouseleave(function() {
+
+      $(".h1").removeClass("showw");
+      $(".h1").addClass("hide");
+
+
+      // business
+   });  $(".p2").mouseenter(function() {
+
+      $(".h2").removeClass("hide");
+      $(".h2").addClass("showw");
+   });
+
+   $(".p2").mouseleave(function() {
+
+      $(".h2").removeClass("showw");
+      $(".h2").addClass("hide");
+
+
+
+      // futura
+   });  $(".p3").mouseenter(function() {
+
+      $(".h3").removeClass("hide");
+      $(".h3").addClass("showw");
+   });
+
+   $(".p3").mouseleave(function() {
+
+      $(".h3").removeClass("showw");
+      $(".h3").addClass("hide");
+   });
+
+   // book
+   $(".p4").mouseenter(function() {
+
+      $(".h4").removeClass("hide");
+      $(".h4").addClass("showw");
+   });
+
+   $(".p4").mouseleave(function() {
+
+      $(".h4").removeClass("showw");
+      $(".h4").addClass("hide");
+   });
+
+
+   });
